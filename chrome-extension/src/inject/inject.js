86d5368ac91f757f2efbcd48c98dfc603b2cf14f
brianc <brian.m.carlson@gmail.com>
2015-04-22 23:04:15 -0400

    Button text

--- chrome-extension/src/inject/inject.js
+++ chrome-extension/src/inject/inject.js
@@ -1,2 +1,5 @@
-window.addEventListener('load', function() {
-});
+var tid = setInterval(function() {
+  //$('button.merge-branch-action').hide();
+  var $button = $('button.merge-branch-action');
+  $button.html('<span class="octicon octicon-thumbsup"></span> Rebase + Merge');
+}, 100);
